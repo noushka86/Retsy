ba409e44cda6ee4a5fb1a3dedc39cc23e232e167
Noa Iolin <be.happy.be1986@gmail.com>
2015-10-21 10:46:20 -0500

    fixing errors

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -10,7 +10,7 @@ let fetch = require('./fetcher'),
 
 console.log("jS loaded")
 
-// import HomeView from "./homeView.js"
+import HomeView from "./homeView.js"
 // import DetailsView from "./detailsView.js"
 import UpperPanel from "./upperPanel.js"
 
@@ -34,112 +34,6 @@ url: "https://openapi.etsy.com/v2/listings/",
 apiKey: "v4scom4wfeznsgrvftmz39if"
 })
 
-//////////////////ROUTER//////////////////////
-var EtsyRouter=Backbone.Router.extend({
-
-	routes:{
-		'home':'showHomeView',
-		'details/:shop/:itemId':'showDetails',
-		'search/:keyword':'showSearchResults',
-		'favorites':'showFavorites',
-		'login':'showLogin',
-		'signup':'showSignUp',
-		 '*anyroute':'changeHash'
-
-	},
-
-	showLogin:function(){
-		// React.render(<LoginView />, document.querySelector('#container'))
-	},
-
-	// showSignUp:function(){
-		
-	// },
-
-
-	showFavorites:function(){
-		var query=new Parse.Query('Listing')
-		query.find().then(this.renderFavorites.bind(this))
-	},
-
-	renderFavorites:function(responseData){
-		console.log('xxxxxx')
-		console.log(responseData)
-		React.render(<FavoritesView listings={responseData}/>, document.querySelector('#container'))
-	},
-
-	showSearchResults:function(keyword){
-		var url="https://openapi.etsy.com/v2/listings/active.js?kewords="+keyword
-		this.doFilterFetch("etsy",url,"Images",keyword).done(this.renderHome.bind(this))
-	},
-
-	doFilterFetch:function(entity,url,includeImage,keyword){
-		return this[entity].fetch({
-			url: url,
-			dataType: 'jsonp',
-			data:{
-				keywords:keyword,
-				"api_key":"v4scom4wfeznsgrvftmz39if",
-				"includes":includeImage+",Shop"
-			},
-			processData: true
-		})
-	},
-
-	doFetch:function(entity,url,includeImage){
-		
-		return this[entity].fetch({
-			url: url,
-			dataType: 'jsonp',
-			data:{
-
-				"api_key":"v4scom4wfeznsgrvftmz39if",
-				"includes":includeImage+",Shop"
-			},
-			processData: true
-		})
-		
-		
-	},
-
-	showDetails:function(shop,listingsId){
-		var url=`https://openapi.etsy.com/v2/listings/${listingsId}.js`
-		this.doFetch("item",url,"MainImage").done(this.renderDetails.bind(this))
-		
-	},
-
-	renderDetails:function(){
-		React.render(<DetailsView listing={this.item.attributes.results}/>, document.querySelector('#container'))
-				
-
-	},
-
-	changeHash:function(){
-    	location.hash='home';
-    },
-
-	showHomeView:function(){
-		this.doFetch("etsy","https://openapi.etsy.com/v2/listings/active.js","Images")
-		.done(this.renderHome.bind(this))
-
-	},
-
-	renderHome:function(){
-		React.render(<HomeView listings={this.etsy.models[0]
-			.attributes.results}/>,document.querySelector('#container'))
-		
-
-	},
-
-	initialize:function(){
-		this.etsy=new EtsyCollection();
-		this.item=new ItemModel();
-		Backbone.history.start();
-	}
-})
-
-var router=new EtsyRouter();
-//////////////////////////////////////////////////////////////////
 
 
 
@@ -372,83 +266,109 @@ var Listing=React.createClass({
 
 
 
-var HomeView=React.createClass({
-	render:function(){
-		console.log(this.props.listings)
-		return (
-				<div id="home">
-					<UpperPanel/>
-					<ListingsContainer listings={this.props.listings}/>
-				</div>
-			)
-	}
-})
+//////////////////ROUTER//////////////////////
+var EtsyRouter=Backbone.Router.extend({
 
+	routes:{
+		'home':'showHomeView',
+		'details/:shop/:itemId':'showDetails',
+		'search/:keyword':'showSearchResults',
+		'favorites':'showFavorites',
+		'login':'showLogin',
+		'signup':'showSignUp',
+		 '*anyroute':'changeHash'
 
-var Listing=React.createClass({
-
-	_clickHandler:function(event)
-	{
-		location.hash="details/"+this.props.listingData.Shop.shop_name+"/"+this.props.listingData.listing_id
 	},
 
-	_getImgSource: function(listingData) {
-        if (!listingData['Images']) { // didnt solve the problem!
-            return "https://vignette1.wikia.nocookie.net/thefakegees/images/8/86/Il_170x135.434854012_ipww.jpg"
-        } else {
-            return listingData.Images[0].url_570xN
-        }
+	showLogin:function(){
+		// React.render(<LoginView />, document.querySelector('#container'))
+	},
+
+	// showSignUp:function(){
+		
+	// },
+
+
+	showFavorites:function(){
+		var query=new Parse.Query('Listing')
+		query.find().then(this.renderFavorites.bind(this))
+	},
+
+	renderFavorites:function(responseData){
+		console.log('xxxxxx')
+		console.log(responseData)
+		React.render(<FavoritesView listings={responseData}/>, document.querySelector('#container'))
+	},
+
+	showSearchResults:function(keyword){
+		var url="https://openapi.etsy.com/v2/listings/active.js?kewords="+keyword
+		this.doFilterFetch("etsy",url,"Images",keyword).done(this.renderHome.bind(this))
+	},
+
+	doFilterFetch:function(entity,url,includeImage,keyword){
+		return this[entity].fetch({
+			url: url,
+			dataType: 'jsonp',
+			data:{
+				keywords:keyword,
+				"api_key":"v4scom4wfeznsgrvftmz39if",
+				"includes":includeImage+",Shop"
+			},
+			processData: true
+		})
+	},
+
+	doFetch:function(entity,url,includeImage){
+		
+		return this[entity].fetch({
+			url: url,
+			dataType: 'jsonp',
+			data:{
+
+				"api_key":"v4scom4wfeznsgrvftmz39if",
+				"includes":includeImage+",Shop"
+			},
+			processData: true
+		})
+		
+		
+	},
+
+	showDetails:function(shop,listingsId){
+		var url=`https://openapi.etsy.com/v2/listings/${listingsId}.js`
+		this.doFetch("item",url,"MainImage").done(this.renderDetails.bind(this))
+		
+	},
+
+	renderDetails:function(){
+		React.render(<DetailsView listing={this.item.attributes.results}/>, document.querySelector('#container'))
+				
+
+	},
+
+	changeHash:function(){
+    	location.hash='home';
     },
 
-    	_getTitle:function(listingData){
-		return listingData.title
-	},
-	_getPrice:function(listingData){
-		var symbols = {
-            USD: "$",
-            GBP: "£",
-            EUR: "€",
-            AUD: "AU$"
-        }
-        return symbols[listingData.currency_code] + listingData.price + " " + listingData.currency_code
+	showHomeView:function(){
+		this.doFetch("etsy","https://openapi.etsy.com/v2/listings/active.js","Images")
+		.done(this.renderHome.bind(this))
 
 	},
 
-	render:function(){
-		return(
-			 <div onClick={this._clickHandler} className="listing">	
-			 	<img className="image" src={this._getImgSource(this.props.listingData)}/>
-			 	<p className="title">{this._getTitle(this.props.listingData)}</p>
-			 	<p className="price">{this._getPrice(this.props.listingData)}</p>
-			 </div>
-			)
+	renderHome:function(){
+		React.render(<HomeView listings={this.etsy.models[0]
+			.attributes.results}/>,document.querySelector('#container'))
+		
+
+	},
+
+	initialize:function(){
+		this.etsy=new EtsyCollection();
+		this.item=new ItemModel();
+		Backbone.history.start();
 	}
 })
 
-
-var ListingsContainer = React.createClass({
-
-	_getListing: function(listingObj){
-		return (<Listing listingData={listingObj}/>)
-	},
-
-	render:function(){
-		var listings = this.props.listings;
-		console.log(listings)
-
-		return(
-			<div id="listingsContainer">
-				{
-					listings.map(this._getListing)
-				} 
-			</div>
-			
-			)		
-	}
-})
-
-
-
-
-
-
+var router=new EtsyRouter();
+//////////////////////////////////////////////////////////////////
